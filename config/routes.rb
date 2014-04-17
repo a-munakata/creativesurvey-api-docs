a5c8970040b82fedbbba9fff56048f33d0dd12cd
a-munakata <a.munakata@4digit.jp>
2014-04-17 15:15:48 +0900

    ルーティングの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Docs::Application.routes.draw do
   resources :docs
   resources :entries
-  resources :variables
   resources :categories
-
-  get "preview", :to => "docs#preview"
-
-
-  get "edit",  :to =>"docs#edit"
-  post "edit", :to => "docs#save"
-
-  get "add_variable", :to => "variables#add"
-  get "add_entry",    :to => "entries#add"
-  get "add_category", :to => "categories#add"
-
-  get "redraw_page",  :to => "docs#redraw_page"
 
   root :to => "docs#index"
 end
